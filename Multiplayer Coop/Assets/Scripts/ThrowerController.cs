b1124973c2c539d6aa4370282ebfbb6b745d72b9
Dkhoan <khoan8@uci.edu>
2018-04-18 13:45:04 -0700

    ...

--- Multiplayer Coop/Assets/Scripts/ThrowerController.cs
+++ Multiplayer Coop/Assets/Scripts/ThrowerController.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class ThrowerController : MonoBehaviour {
 
     // Use this for initialization
-    public float force;
+    #region Attributes
+    private const float Max_Angle = 90.0f;
+    private const float Min_Angle = 0.0f;
+    public float Angle;
     private bool canThrow;
     public Camera PlayerCam;
     public GameObject throwable_Object;
     public GameObject Player;
-    public int constant;
-	void Start () {
+    public GameObject Arrow;
+    public int ThrowForce; 
+    #endregion
+
+    void Start () {
+        Angle = 45;
+        ThrowForce = 10;
+        Arrow.transform.position = gameObject.transform.position + Vector3.up * 2;
+        Arrow.transform.rotation = gameObject.transform.rotation;
         canThrow = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Angle += -Input.GetAxis("Mouse ScrollWheel") * 3;
+        Angle = Mathf.Clamp(Angle, Min_Angle, Max_Angle);
+        Arrow.transform.position = gameObject.transform.position + Vector3.up * 2;
+        Arrow.transform.rotation = gameObject.transform.rotation * Quaternion.Euler(-Angle,0,0);
         if (Input.GetMouseButtonDown(0))
         {
             if (canThrow)
@@ -26,13 +40,18 @@ public class ThrowerController : MonoBehaviour {
         }
 	}
 
-    protected void Throw()
+    private void SpawnThrowAngle()
+    {
+
+    }
+
+    private void Throw()
     {
         GameObject throwO = Instantiate(throwable_Object, Player.transform.position + Vector3.up * 2, Player.transform.rotation);
         throwO.GetComponent<Rigidbody>().AddForce
         (
-            (throwO.transform.forward * Mathf.Cos(Mathf.Deg2Rad * force) + 
-            throwO.transform.up * Mathf.Sin(Mathf.Deg2Rad * force)) * constant,
+            (throwO.transform.forward * Mathf.Cos(Mathf.Deg2Rad * Angle) + 
+            throwO.transform.up * Mathf.Sin(Mathf.Deg2Rad * Angle)) * ThrowForce,
             ForceMode.Impulse
         );
     }
