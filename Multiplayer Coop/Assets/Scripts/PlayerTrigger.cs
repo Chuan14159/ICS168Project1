f9efb8276f4d91fc1c9d19da43b5297de1c9a15f
Changchuan Shen <changchs@uci.edu>
2018-04-18 17:18:01 -0700

    F for Pickup and Putdown
    Players are now able to pick up objects and put down objects in the front by pressing F.

---
+++ Multiplayer Coop/Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour {
+
+    private bool isPicking = false;
+    private GameObject picked;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log(isPicking);
+        if(other.tag == "Object")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                isPicking = !isPicking;
+                if (isPicking)
+                {
+                    picked = other.gameObject;
+                }
+            }
+            if (isPicking)
+            {
+                //Debug.Log("picking");
+                picked.transform.position = transform.position;
+                picked.transform.rotation = transform.rotation;
+            }
+        }
+    }
+}
